6283530d955d63877b3e7582aeb7311ff1640c2f
Jeff Dunn <jdunn522@gmail.com>
2015-10-26 15:34:51 -0700

    Complete assignment for 10/26/2015

--- 20151026_simple_test_and_modular_patterns/greet.js
+++ 20151026_simple_test_and_modular_patterns/greet.js
@@ -2,8 +2,7 @@ module.exports.greet = function (name) {
   return 'hello ' + name
 }
 
-// For an extra point, create a command line utility that will be run using node greet.js 'some name' and will pass the input contained in that argument to the greet function and output
-// the result to the screen.
+// For an extra point, create a command line utility that will be run using node greet.js 'some name' and will pass the input contained in that argument to the greet function and output the result to the screen.
 
 function reportInput () {
   var input = process.argv[2]
